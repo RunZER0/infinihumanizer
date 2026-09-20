4d4e2cb4dafaa810ce5892559fa7e593ad8b498f
Victor Mawira <vikkicleo@gmail.com>
2026-09-20 17:34:29 +0300

    Humanizer: remove legacy routes

--- humanizer/urls.py
+++ humanizer/urls.py
@@ -1,28 +1,8 @@
 from django.urls import path
-from .views import (
-    humanizer_view,
-    humanize_ajax,
-    get_modes,
-    get_models,
-    pricing_view,
-    about_view,
-    contact_view,
-    settings_view,
-    devices_view,
-    start_payment,
-    verify_payment
-)
+
+from .views import humanize_ajax, humanizer_view
 
 urlpatterns = [
-    path('', humanizer_view, name='humanizer'),
-    path('humanize/', humanize_ajax, name='humanize_ajax'),
-    path('api/modes/', get_modes, name='get_modes'),
-    path('api/models/', get_models, name='get_models'),
-    path('pricing/', pricing_view, name='pricing'),
-    path('about/', about_view, name='about'),
-    path('contact/', contact_view, name='contact'),
-    path('settings/', settings_view, name='settings'),
-    path('devices/', devices_view, name='devices'),
-    path('start-payment/', start_payment, name='start-payment'),
-    path('verify-payment/', verify_payment, name='verify-payment'),
+    path("", humanizer_view, name="humanizer"),
+    path("humanize/", humanize_ajax, name="humanize_ajax"),
 ]
