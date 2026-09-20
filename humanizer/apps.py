5f0000cc32d881480dd835865094e5d90b2bc5be
Victor Mawira <vikkicleo@gmail.com>
2026-09-20 17:39:10 +0300

    Cleanup: remove legacy humanizer startup hooks

--- humanizer/apps.py
+++ humanizer/apps.py
@@ -2,9 +2,5 @@ from django.apps import AppConfig
 
 
 class HumanizerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'humanizer'
-    
-    def ready(self):
-        """Initialize NLTK data when Django starts"""
-        pass  # NLTK punkt will be downloaded on first use if needed
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "humanizer"
