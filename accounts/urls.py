5fe99eef6250573ba1d8c5ea128621f632d1e82f
Victor Mawira <vikkicleo@gmail.com>
2026-09-21 08:23:15 +0300

    OAuth: route Google through fixed callback adapter

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from .google_oauth import google_callback, google_login
 from .views import VerifiedEmailLoginView, resend_verification, signup_view
 
 urlpatterns = [
+    path("google/login/", google_login, name="google_login_fixed"),
+    path("google/login/callback/", google_callback, name="google_callback"),
     path("signup/", signup_view, name="account_signup"),
     path("login/", VerifiedEmailLoginView.as_view(), name="account_login"),
     path("resend-verification/", resend_verification, name="resend_verification"),
