c92698ad5fc73d3489a18e9eb4a2e556a3dcf06b
Victor Mawira <vikkicleo@gmail.com>
2026-09-21 10:19:44 +0300

    Add humanizer save endpoint

--- humanizer/urls.py
+++ humanizer/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import humanize_ajax, humanizer_view
+from .views import humanize_ajax, humanizer_view, save_humanization
 
 urlpatterns = [
     path("", humanizer_view, name="humanizer"),
     path("humanize/", humanize_ajax, name="humanize_ajax"),
+    path("save/", save_humanization, name="save_humanization"),
 ]
